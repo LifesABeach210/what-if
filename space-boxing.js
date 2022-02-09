00159ce7b79f7a13765a38159709684826f03f63
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-02-08 20:04:42 -0500

    hello

---
+++ space-boxing.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+let weight= Number(prompt("Enter weight"))
+let planet = Number(prompt("enter your planet"));
+
+if (planet === "venus") {console.log(weight*.78)}
+
+else if (planet === "mars")  result = weight*.39; {console.log(weight*.39);}
+
+   if (planet === "jupiter")  result = weight*2.65; {console.log(weight*2.65);}
+
+   if (planet === "saturn")  result = weight*1.17; {console.log(weight*1.17);}
+
+   if (planet === "uranus")  result = weight*1.05;{console.log(weight*1.05)}
+
+  if (planet === "nuptune")  result = weight * 1.23; {console.log(weight*1.23)}
+
+  {console.log("error")}
+
+
+
+
+
+
+
